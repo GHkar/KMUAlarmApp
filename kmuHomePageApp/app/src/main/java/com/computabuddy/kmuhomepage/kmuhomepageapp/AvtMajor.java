1fd6186e378a279aadca18cbe25d1d8515e81736
KAR <dofl0119@gmail.com>
2018-11-05 20:07:25 +0900

    [App] - 학과 액비티비 생성 - 글쓰기 버튼 추가

---
+++ kmuHomePageApp/app/src/main/java/com/computabuddy/kmuhomepage/kmuhomepageapp/AvtMajor.java
@@ -0,0 +1,13 @@
+package com.computabuddy.kmuhomepage.kmuhomepageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvtMajor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avt_major);
+    }
+}
